db7adc5dc2dbdc083fec66e35b5a56632ceed231
khermstad <krishermstad@gmail.com>
2019-03-10 14:49:42 -0400

    added readme for testing teamcity ide integration

--- src/main/java/com/khermstad/userauth/service/UserService.java
+++ src/main/java/com/khermstad/userauth/service/UserService.java
@@ -2,8 +2,6 @@ package com.khermstad.userauth.service;
 
 import com.khermstad.userauth.entity.User;
 import com.khermstad.userauth.repository.UserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
